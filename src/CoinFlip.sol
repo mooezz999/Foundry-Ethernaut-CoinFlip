9161b9f6bb4af76c30c1a993678cb12e6a36bd12
mooezz999 <moustafaezz999@gmail.com>
2022-09-24 00:20:57 +0200

    second

--- src/CoinFlip.sol
+++ src/CoinFlip.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/math/SafeMath.sol";
+import "openzeppelin-contracts/contracts/math/SafeMath.sol";
 
 contract CoinFlip {
     using SafeMath for uint256;
